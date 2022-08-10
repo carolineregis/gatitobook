9c47fee8cc983a11874ab5c3916977134bed7e92
Carol <caroline.regis@devmagic.com.br>
2022-08-10 18:45:09 -0300

    WIP(*): implementing login component - added route to new page 'animais' - added validators to login form - updated home.module - created component to show validation message - created component to show list of animals - updated app routing module

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = ''
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(private authService: AutenticacaoService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -20,6 +21,7 @@ export class LoginComponent implements OnInit {
     this.authService.autenticar(this.usuario, this.senha).subscribe(
       () => {
         console.log('Autenticado com sucesso');
+        this.router.navigate(['animais'])
       },
       (error) => {
         alert("Usuário ou senha inválido")
